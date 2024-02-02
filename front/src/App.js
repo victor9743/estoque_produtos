c377f6e4b74154dc7b0fd431de62305cb642b2ed
Victor Costa <70709626+victor9743@users.noreply.github.com>
2024-02-02 13:47:21 -0300

    front/react
    criação do front

---
+++ front/src/App.js
@@ -0,0 +1,9 @@
+import Inicial from "./pages";
+
+function App() {
+  return (
+    <Inicial />
+  );
+}
+
+export default App;
---
+++ front/src/pages/index.jsx
@@ -0,0 +1,15 @@
+function Inicial() {
+    return (
+        <>
+            <h4>Produtos</h4>
+            <table className="table table-bordered">
+                <thead>
+                    <th>#</th>
+                    <th>Nome</th>
+                </thead>
+            </table>
+        </>
+    )
+}
+
+export default Inicial;
